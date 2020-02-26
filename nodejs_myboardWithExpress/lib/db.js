69649cd8fdd7fd0ef127978ccb35df105af05b7a
백승훈 <46189249+Walkers15@users.noreply.github.com>
2020-02-26 14:12:11 +0900

    Delete db.js

--- nodejs_myboardWithExpress/lib/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '111111',
-	database: 'myboard'
-});
-db.connect();
-module.exports = db;
